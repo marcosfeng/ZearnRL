563d14616dba076e91be023d48dc8c13edfa2908
Marcos Gallo <42765589+marcosfeng@users.noreply.github.com>
2023-10-11 11:06:47 -0400

    Finding top Actor-Critic models complete

--- CBM/zearn_models/refine_ac.m
+++ CBM/zearn_models/refine_ac.m
@@ -1,3 +1,5 @@
+%% Re-estimate top 5 models with more precision
+
 % Add paths
 addpath(fullfile('..','codes'));
 addpath(fullfile('..','zearn_models','wrappers'));
@@ -44,6 +46,8 @@ parfor i = 1:5
     cbm_lap(data, models{i}, prior_ac, fname{i},pconfig);
 end
 
+%% Find top 2 models by valid log evidence and re-estimate with new priors
+
 valid_subj_all = ones(1,210);
 for i = 1:5
     % Load the saved output for this model
@@ -64,13 +68,67 @@ for i = 1:5
     valid_subj_all = valid_subj_all & valid_subjects;
 end
 
-% Filter out invalid subjects from the original data
-filtered_data = data(valid_subj_all);
-filtered_name = cell(1,5);
+log_evidence = zeros(1, 5);
 for i = 1:5
-    % Load the saved output for this model
     loaded_data = load(fname{i});
+    log_evidence(i) = ...
+        sum(loaded_data.cbm.output.log_evidence(valid_subj_all));
+end
+
+[~, top2_indices] = sort(log_evidence, 'descend');
+top2_indices = top2_indices(1:2);
+% Create the prior structure for your new model
+v = 6.25;
+num_parameters = 9;
+% Create the prior structure from previous estimation
+prior_ac = struct('mean', zeros(num_parameters, 1), 'variance', v);
+top_models = cell(1,2);
+top_fname = cell(1,2);
+for i = 1:2
+    top_models{i} = str2func(sprintf('wrapper_function_%d', top5_indices(top2_indices(i))));
+    top_fname{i} = sprintf('ac_refine/refine_ac_%d.mat', top5_indices(top2_indices(i)));
+
+    loaded_data = load(top_fname{i});
+    prior_ac.mean = prior_ac.mean + ...
+        mean(loaded_data.cbm.output.parameters,1)';
+    top_fname{i} = sprintf('ac_refine/top2_ac_%d.mat', top5_indices(top2_indices(i)));
+end
+prior_ac.mean = prior_ac.mean/2;
+
+% Populate the top 2 models and their corresponding fcbm_maps
+parfor i = 1:2
+    % Run the cbm_lap function for your new model
+    cbm_lap(data, top_models{i}, prior_ac, top_fname{i},pconfig);
+end
+
+%% Run cbm_hbi for top models
+
+% Filter out invalid subjects from the original data
+valid_subj_all = ones(1,210);
+for i = 1:2
+    % Load the saved output for this model
+    loaded_data = load(top_fname{i});
     
+    % Initialize a logical index for valid subjects
+    valid_subjects = ~isnan(loaded_data.cbm.math.logdetA) ...
+        & ~isinf(loaded_data.cbm.math.logdetA) ...
+        & (loaded_data.cbm.math.logdetA ~= 0);
+    % Calculate the mean and standard deviation of logdetA for valid subjects
+    mean_logdetA = mean(loaded_data.cbm.math.logdetA(valid_subjects));
+    std_logdetA = std(loaded_data.cbm.math.logdetA(valid_subjects));
+    
+    % Add the condition for logdetA values within 3 standard deviations of the mean
+    valid_subjects = valid_subjects & ...
+        (abs(loaded_data.cbm.math.logdetA - mean_logdetA) <= 3 * std_logdetA);
+
+    valid_subj_all = valid_subj_all & valid_subjects;
+end
+filtered_data = data(valid_subj_all);
+filtered_name = cell(1,2);
+for i = 1:2
+    % Load the saved output for this model
+    loaded_data = load(top_fname{i});
+
     loaded_data.cbm.profile.optim.flag = ...
         loaded_data.cbm.profile.optim.flag(valid_subj_all);
     loaded_data.cbm.profile.optim.gradient = ...
@@ -91,60 +149,30 @@ for i = 1:5
         loaded_data.cbm.output.log_evidence(valid_subj_all);
     loaded_data.cbm.output.parameters = ...
         loaded_data.cbm.output.parameters(valid_subj_all, :);
-    
+
     % Save the modified loaded_data back to the same file
-    filtered_name{i} = sprintf('ac_refine/filtered_ac_%d.mat', top5_indices(i));
+    filtered_name{i} = sprintf('ac_refine/filtered_ac_%d.mat', top5_indices(top2_indices(i)));
     save(filtered_name{i}, '-struct', 'loaded_data');
 end
-
-log_evidence = zeros(1, 5);
-for i = 1:5
-    loaded_data = load(fname{i});
-    log_evidence(i) = ...
-        sum(loaded_data.cbm.output.log_evidence(valid_subj_all));
-end
-% Rank the models by log evidence and get the indices of the top 2
-[~, top2_indices] = sort(log_evidence, 'descend');
-top2_indices = top2_indices(1:2);
-% Create the prior structure for your new model
-v = 2;
-num_parameters = 9;
-% Create the prior structure from previous estimation
-prior_ac = struct('mean', zeros(num_parameters, 1), 'variance', v);
-for i = 1:2
-    models{i} = str2func(sprintf('wrapper_function_%d', top5_indices(top2_indices(i))));
-    fname{i} = sprintf('ac_refine/refine_ac_%d.mat', top5_indices(top2_indices(i)));
-
-    loaded_data = load(fname{top5_indices(top2_indices(i))});
-    prior_ac.mean = prior_ac.mean + ...
-        mean(loaded_data.cbm.output.parameters,1)';
-    fname{i} = sprintf('ac_refine/top2_ac_%d.mat', top5_indices(top2_indices(i)));
-end
-prior_ac.mean = prior_ac.mean/2;
-
-% Populate the top 2 models and their corresponding fcbm_maps
-parfor i = 1:2
-    % Run the cbm_lap function for your new model
-    cbm_lap(data, models{i}, prior_ac, fname{top5_indices(top2_indices(i))});
-end
-
-% Run cbm_hbi for all models
 fname_hbi = 'hbi_AC_refined.mat';
-cbm_hbi(data, models(1:2), filtered_name, fname_hbi);
+cbm_hbi(filtered_data, top_models, filtered_name, fname_hbi);
 
 % Load the HBI results and store them
 fname_hbi_loaded = load(fname_hbi);
 hbi_results = fname_hbi_loaded.cbm;
 hbi_results.output
 
-model_names = {'Ba, M', 'A, Ba, M', 'A, Bo, M', 'Ba, Bo, M'};
-param_names = {'\alpha_w','\alpha_\theta','\gamma', ...
-    '\tau', 'c_1', 'c_2', 'c_3'};
+model_names = {'ASt ~ Ba, Bo, M', 'Ba ~ ASt, A, M'};
+param_names = {'\alpha_W','\alpha_\theta','\gamma', ...
+    '\tau', '\theta_0', 'W_0', ...
+    'c_1', 'c_2', 'c_3'};
 % note the latex format
 % transformation functions associated with each parameter
-transform = {'exp','exp','sigmoid','exp','exp','exp','exp'};
+transform = {'sigmoid','sigmoid','sigmoid', ...
+    'exp', 'none', 'none', ...
+    'exp','exp','exp'};
 
-cbm_hbi_plot(fname_hbi, model_names, param_names, transform)
+cbm_hbi_plot(fname_hbi, model_names, param_names, transform);
 % this function creates a model comparison plot
 % (exceednace probability and model frequency)
 % plot of transformed parameters of the most frequent model
