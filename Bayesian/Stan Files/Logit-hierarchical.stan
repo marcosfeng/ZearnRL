8098068ae8ad6939ab1b1e9424d196a4e6ffa4b1
Marcos Gallo <42765589+marcosfeng@users.noreply.github.com>
2023-11-03 11:33:54 -0400

    Updating hierarchical models

---
+++ Bayesian/Stan Files/Logit-hierarchical.stan
@@ -0,0 +1,73 @@
+data {
+  int<lower=0> N;  // number of observations
+  int<lower=0> C;  // number of classrooms
+  matrix[N, 3] X;  // predictor matrix
+  array[N, 3] int<lower=0, upper=1> y;  // response variables
+  array[N]    int<lower=1, upper=C> classroom;  // classroom identifier
+  int<lower=1> number_teachers; // Number of unique teachers
+  array[N] int<lower=1> group; // Teacher ID for each classroom
+}
+transformed data {
+  array[C] int counts = rep_array(0, C);  // initialize counts
+  int T;
+  for (n in 1:N) {
+    counts[classroom[n]] += 1;  // increment count for the classroom of the nth observation
+  }
+  T = max(counts);  // find the maximum count
+}
+parameters {
+  array[3] real mu_alpha;           // group-level mean for alpha
+  array[3] real<lower=0> sigma_alpha;   // group-level sd for alpha
+  array[3] vector[3] mu_beta;         // group-level mean for beta
+  array[3] vector<lower=0>[3] sigma_beta; // group-level sd for beta
+
+  array[3, number_teachers] real alpha;   // classroom-specific alphas
+  array[3, 3, number_teachers] real beta; // classroom-specific betas
+}
+model {
+  int teacher;
+  mu_alpha ~ normal(0, 1);
+  sigma_alpha ~ cauchy(0, 2.5);
+
+  for (i in 1:3) {
+    mu_beta[i] ~ normal(0, 1);
+    sigma_beta[i] ~ cauchy(0, 2.5);
+  }
+  for (c in 1:number_teachers) {
+    alpha[, c] ~ normal(mu_alpha, sigma_alpha);
+    for (j in 1:3) {
+      beta[j, , c] ~ normal(mu_beta[j], sigma_beta[j]);
+    }
+  }
+
+  for (n in 1:N) {
+    teacher = group[classroom[n]];
+    for (j in 1:3) {
+      y[n, j] ~ bernoulli_logit(alpha[j, teacher]
+                + dot_product(X[n,], to_vector(beta[j, , teacher])));
+    }
+  }
+}
+generated quantities {
+  int teacher;
+  array[C, T, 3] real y_pred = rep_array(-1, C, T, 3);
+  // For log likelihood calculation
+  vector[C] log_lik;  // log likelihood per classroom
+  int k;
+  array[C] int week = rep_array(0, C);
+
+  for (c in 1:C) {
+    log_lik[c] = 0;  // initialize log likelihood for each classroom
+  }
+
+  for (n in 1:N) {
+    k = classroom[n];
+    teacher = group[classroom[n]];
+    week[k] += 1;
+    for (j in 1:3) {
+      y_pred[k, week[k], j] = inv_logit(alpha[j, teacher]
+                              + dot_product(X[n,], to_vector(beta[j, , teacher])));
+      log_lik[k] += bernoulli_lpmf(y[n, j] | y_pred[k, week[k], j]);
+    }
+  }
+}
--- Bayesian/code.R
+++ Bayesian/code.R
@@ -262,9 +262,75 @@ for (choice in names(choices)) {
 }
 
 
-# 4. Hierarchical Models --------------------------------------------------
+# 4. Logit models -----------------------------------------------------------
+
+# Function to find lag
+get_lag_value <- function(datatable, col, lag_period, n_comp = NULL) {
+  # Convert to data.table if it's a data.frame
+  if (is.data.frame(datatable)) {
+    datatable <- as.data.table(datatable)
+  }
+
+  # Add a column for week_lag
+  week_lag <- c(0, diff(datatable$week))
+  set(datatable, j = "week_lag", value = week_lag)
+
+  if (is.null(n_comp)) {
+    # Update the lag column with shift function
+    new_col_name <- paste0(col, "_", lag_period)
+    new_col_value <- shift(datatable[[col]], lag_period, fill = 0, type = "lag")
+    set(datatable, j = new_col_name, value = new_col_value)
+  } else {
+    for (comp in 1:n_comp) {
+      # Update the lag column with shift function
+      new_col_name <- paste0(col, comp, "_", lag_period)
+      new_col_value <- shift(datatable[[paste0(col, comp)]], lag_period, fill = 0, type = "lag")
+      set(datatable, j = new_col_name, value = new_col_value)
+    }
+  }
+
+  return(datatable)
+}
+
+df <- df %>%
+  mutate(state_logit = case_when(is.na(state) ~ 0,
+                                 .default = state - 1))
+
+# Non-hierarchical models
+for (col in c(paste0("FrobeniusNNDSVD", 1:3, "bin"),"Badges.per.Active.User")) {
+  df <- get_lag_value(df, col, 1)
+}
+# Prepare data for Stan
+stan_data <- list(
+  C = length(unique(df$Classroom.ID)), # Number of classrooms
+  N = nrow(df),
+  X = df %>% select(FrobeniusNNDSVD1bin_1, Badges.per.Active.User_1, state_logit) %>% as.matrix(),
+  y = df %>% select(FrobeniusNNDSVD1bin, FrobeniusNNDSVD2bin, FrobeniusNNDSVD3bin) %>% as.matrix(),
+  classroom = df %>%
+    mutate(classroom = as.integer(factor(Classroom.ID))) %>%
+    select(classroom) %>% unlist() %>% as.integer()
+)
+
+# Compile the Stan model
+logistic_model <- cmdstan_model("Bayesian/Stan Files/Logit.stan")
+
+# Fit the model
+fit <- logistic_model$sample(
+  data = stan_data,
+  chains = 3,
+  parallel_chains = 3,
+  iter_warmup = 2500,
+  iter_sampling = 2500
+)
+
+# Save the fit object
+fit$save_object(file = "Bayesian/Results/logit.RDS")
+
+
+# 5. Hierarchical Models --------------------------------------------------
 
 df[, Teacher.Rank := .GRP, by = .(Teacher.User.ID)]
+
 
 ## Q-learning
 
@@ -329,57 +395,97 @@ for (choice in names(choices)) {
 }
 
 
-# 5. Logit models -----------------------------------------------------------
+## Actor Critic
 
-# Function to find lag
-get_lag_value <- function(datatable, col, lag_period, n_comp = NULL) {
-  # Convert to data.table if it's a data.frame
-  if (is.data.frame(datatable)) {
-    datatable <- as.data.table(datatable)
-  }
+# Prepare list of models
+models <- c("Bayesian/Stan Files/Actor-Critic-hierarchical.stan")
 
-  # Add a column for week_lag
-  week_lag <- c(0, diff(datatable$week))
-  set(datatable, j = "week_lag", value = week_lag)
+# Generate Stan data, fit model, and save for each choice array
+for (choice in names(choices)) {
+  for (model in models) {
+    print(paste0("Processing model: ", model, " with choice array: ", choice))
 
-  if (is.null(n_comp)) {
-    # Update the lag column with shift function
-    new_col_name <- paste0(col, "_", lag_period)
-    new_col_value <- shift(datatable[[col]], lag_period, fill = 0, type = "lag")
-    set(datatable, j = new_col_name, value = new_col_value)
-  } else {
-    for (comp in 1:n_comp) {
-      # Update the lag column with shift function
-      new_col_name <- paste0(col, comp, "_", lag_period)
-      new_col_value <- shift(datatable[[paste0(col, comp)]], lag_period, fill = 0, type = "lag")
-      set(datatable, j = new_col_name, value = new_col_value)
+    choice_array <- prepare_choice_array(df,
+                                         choices[[choice]][1],
+                                         choices[[choice]][2],
+                                         choices[[choice]][3])
+
+    # Create a 3D array with the correct dimensions
+    state_array <- array(0, dim = c(length(unique(df$Classroom.ID)), max(df$row_n), num_state_var))
+    # Create a vector of unique Classroom.ID values
+    unique_ids <- unique(df$Classroom.ID)
+    # Fill the array with your state data
+    for (i in seq_along(unique_ids)) {
+      current_id <- unique_ids[i]
+      for (j in 1:max(df[df$Classroom.ID == current_id,]$row_n)) {
+        # Assuming df is ordered by Classroom.ID and row_n
+        state_array[i, j, ] <- c(1,
+                                 df[df$Classroom.ID == current_id &
+                                      df$row_n == j,]$tower_state,
+                                 df[df$Classroom.ID == current_id &
+                                      df$row_n == j,]$actst_state)
+      }
     }
+
+    stan_data <- list(
+      N = length(unique(df$Classroom.ID)), # Number of teachers
+      T = max(df[, row_n]), # Maximum Tsubj across all teachers
+      C = dim(choice_array)[3], # Number of choices
+      Tsubj = df[, .N, by = .(Classroom.ID)][,N], # Number of rows by Teacher
+      choice = choice_array,
+      outcome = replace(as.matrix(dcast(df,
+                                        Classroom.ID ~ row_n,
+                                        value.var = "Badges.per.Active.User")), # Outcome matrix
+                        is.na(as.matrix(dcast(df,
+                                              Classroom.ID ~ row_n,
+                                              value.var = "Badges.per.Active.User"))), 0)[,-1],
+      week = replace(as.matrix(dcast(df,
+                                     Classroom.ID ~ row_n,
+                                     value.var = "week")), # Week matrix
+                     is.na(as.matrix(dcast(df,
+                                           Classroom.ID ~ row_n,
+                                           value.var = "week"))), 0)[,-1],
+      S = num_state_var, # Number of states
+      state = state_array,
+      group = unique(df[, .(Classroom.ID, Teacher.User.ID, Teacher.Rank)])$Teacher.Rank,
+      G = length(unique(df$Teacher.User.ID)) #Number of teachers
+    )
+
+    my_model <- cmdstan_model(model)
+
+    fit <- my_model$sample(
+      data = stan_data,
+      chains = 3,
+      parallel_chains = 3,
+      iter_warmup = 2500,
+      iter_sampling = 2500
+    )
+
+    # Save the fit object
+    fit$save_object(file = paste0("Bayesian/Results/",
+                                  gsub("Bayesian/Stan Files/||.stan", "", model),
+                                  "-", choice, ".RDS"))
   }
-
-  return(datatable)
 }
 
-df <- df %>%
-  mutate(state = case_when(is.na(state) ~ 0,
-                           .default = state - 1))
 
-# Non-hierarchical models
-for (col in c(paste0("FrobeniusNNDSVD", 1:3, "bin"),"Badges.per.Active.User")) {
-  df <- get_lag_value(df, col, 1)
-}
+## Logit
+
 # Prepare data for Stan
 stan_data <- list(
   C = length(unique(df$Classroom.ID)), # Number of classrooms
+  number_teachers = length(unique(df$Teacher.User.ID)),
   N = nrow(df),
-  X = df %>% select(FrobeniusNNDSVD1bin_1, Badges.per.Active.User_1, state) %>% as.matrix(),
+  X = df %>% select(FrobeniusNNDSVD1bin_1, Badges.per.Active.User_1, state_logit) %>% as.matrix(),
   y = df %>% select(FrobeniusNNDSVD1bin, FrobeniusNNDSVD2bin, FrobeniusNNDSVD3bin) %>% as.matrix(),
   classroom = df %>%
     mutate(classroom = as.integer(factor(Classroom.ID))) %>%
-    select(classroom) %>% unlist() %>% as.integer()
+    select(classroom) %>% unlist() %>% as.integer(),
+  group = unique(df[, .(Classroom.ID, Teacher.User.ID, Teacher.Rank)])$Teacher.Rank
 )
 
 # Compile the Stan model
-logistic_model <- cmdstan_model("Bayesian/Stan Files/Logit.stan")
+logistic_model <- cmdstan_model("Bayesian/Stan Files/Logit-hierarchical.stan")
 
 # Fit the model
 fit <- logistic_model$sample(
@@ -393,13 +499,9 @@ fit <- logistic_model$sample(
 # Save the fit object
 fit$save_object(file = "Bayesian/Results/logit.RDS")
 
-# Hierarchical models
 
+# Quick diagnostics -------------------------------------------------------
 
-
-
-
-
-#quick diagnostics
 library("shinystan")
-launch_shinystan(fit)
+stanfit <- rstan::read_stan_csv(fit$output_files())
+launch_shinystan(stanfit)
