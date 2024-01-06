84b1bb1f807c703e085f7effd3cba3f9e386ed7d
Marcos Gallo <42765589+marcosfeng@users.noreply.github.com>
2024-01-06 18:44:23 -0500

    Fixing data cleaning

--- Data/cleaning_RL.R
+++ Data/cleaning_RL.R
@@ -10,90 +10,89 @@ classroom_info <- read.csv("Data/Classroom Info 2020-10-09T1617.csv")
 classroom_teacher_lookup <- read.csv("Data/Classroom-Teacher Lookup 2020-10-09T1618.csv")
 school_info <- read.csv("Data/School Info 2020-10-09T1619.csv")
 
+
+# Feature engineering
+
+teacher_usage <- teacher_usage %>%
+  mutate(
+    # Noting week, and year to summarize teacher behavior:
+    week = lubridate::isoweek(Usage.Time),
+    year = lubridate::year(Usage.Time),
+    # Transforming Event.Type for "Resource Downloaded"
+    Event.Type = ifelse(Event.Type == "Resource Downloaded",
+                        paste0("RD.", Curriculum.Resource.Category),
+                        Event.Type)
+  ) %>%
+  mutate(year = ifelse(week == 1 & year == 2019, 2020, year))
+
+# Summarize teacher behavior per week
+teacher_usage_total <- teacher_usage %>%
+  count(Adult.User.ID, Event.Type, week, year) %>%
+  pivot_wider(names_from = Event.Type, values_from = n,
+              values_fill = list(n = 0)) %>%
+  full_join(teacher_usage %>%
+              group_by(Adult.User.ID, week, year) %>%
+              summarize(Minutes.on.Zearn...Total = sum(Minutes.on.Zearn...Total)),
+            by = c("Adult.User.ID", "week", "year"))
+
 # Data cleaning
 
-# Delete classrooms with multiple teachers
+# Classroom exclusion
 classroom_teacher_lookup <- classroom_teacher_lookup %>%
   group_by(Teacher.User.ID) %>%
   mutate(teacher_number_classes = n()) %>%
   ungroup() %>%
   group_by(Classroom.ID) %>%
+  # Delete classrooms with multiple teachers
   filter(n_distinct(Teacher.User.ID) == 1) %>%
-  ungroup()
-
-# Feature engineering
-
-# Extract week, year, wday, hour, and whour information for teacher_usage
-teacher_usage <- teacher_usage %>%
-  mutate(week = week(Usage.Time),
-         year = year(Usage.Time),
-         wday = wday(Usage.Time),
-         hour = hour(Usage.Time),
-         whour = (wday - 1) * 24 + hour)
-
-# Summarize teacher behavior per week
-teacher_usage_total <- teacher_usage %>%
-  mutate(Event.Type = ifelse(Event.Type == "Resource Downloaded",
-                             paste("RD.", Curriculum.Resource.Category, sep = ""), Event.Type)) %>%
-  group_by(Adult.User.ID, Event.Type, week, year) %>%
-  summarise(Freq = n()) %>%
-  pivot_wider(id_cols = c(Adult.User.ID, week, year),
-              names_from = Event.Type,
-              values_from = Freq) %>%
   ungroup() %>%
-  mutate(across(everything(), ~ifelse(is.na(.), 0, .)))
-
-teacher_usage_total <- teacher_usage %>%
-  group_by(Adult.User.ID, week, year) %>%
-  summarise(tch_min = sum(Minutes.on.Zearn...Total)) %>%
-  full_join(teacher_usage_total, by = c("Adult.User.ID", "week", "year"))
+  inner_join(classroom_info %>%
+               group_by(Classroom.ID) %>%
+               # Delete classrooms with multiple schools
+               filter(n_distinct(MDR.School.ID) == 1) %>%
+               ungroup(), by = "Classroom.ID")
 
 # Data merging
+classroom_student_usage <- classroom_student_usage %>%
+  inner_join(classroom_teacher_lookup %>%
+               select(Classroom.ID, Teacher.User.ID),
+             by = c("Classroom.ID")) %>%
+  mutate(week = lubridate::isoweek(Usage.Week),
+         year = lubridate::year(Usage.Week)) %>%
+  mutate(year = ifelse(week == 1 & year == 2019, 2020, year))
 
-# Merge classroom_student_usage with classroom_teacher_lookup
-df <- classroom_student_usage %>%
-  inner_join(classroom_teacher_lookup,
-             by = c("Classroom.ID"),
-             multiple = "all") %>%
-  mutate(week = week(Usage.Week),
-         year = year(Usage.Week)) %>%
-  right_join(classroom_info, by = "Classroom.ID",
-             multiple = "all",
-             relationship = "many-to-many")  %>%
-  right_join(school_info, by = "MDR.School.ID",
-             multiple = "all") %>%
-  # Delete classrooms with multiple schools
-  group_by(Classroom.ID) %>%
-  filter(n_distinct(MDR.School.ID) == 1) %>%
-  ungroup()
+df <- teacher_usage_total %>%
+  filter(Adult.User.ID %in% unique(classroom_teacher_lookup$Teacher.User.ID)) %>%
+  left_join(classroom_teacher_lookup,
+            by = c("Adult.User.ID" = "Teacher.User.ID"),
+            relationship = "many-to-many") %>%
+  full_join(classroom_student_usage,
+            by = c("Classroom.ID", "Adult.User.ID" = "Teacher.User.ID",
+                   "week", "year"))
 
-# Merge df with teacher_usage_total, classroom_info, and school_info
+# Merge school info
 df <- df %>%
-  inner_join(teacher_usage_total,
-             by = c("Teacher.User.ID" = "Adult.User.ID", "week", "year"),
-             multiple = "all") %>%
-  select(-c(NCES.ID,
-            School.Name,
-            District.Rollup.Name,
-            School.Address...City,
-            School.Address...County,
-            School.Address...State)) %>%
-  mutate(
-    Minutes.per.Active.User = as.numeric(Minutes.per.Active.User),
-    Minutes.per.Active.User = if_else(is.na(Minutes.per.Active.User),
-                                      0, Minutes.per.Active.User),
-    Badges.per.Active.User = if_else(is.na(Badges.per.Active.User),
-                                     0, Badges.per.Active.User),
-    Tower.Alerts.per.Tower.Completion = if_else(is.na(Tower.Alerts.per.Tower.Completion),
-                                                0, Tower.Alerts.per.Tower.Completion),
-    Boosts.per.Tower.Completion = if_else(is.na(Boosts.per.Tower.Completion),
-                                          0, Boosts.per.Tower.Completion)) %>%
+  left_join(school_info %>%
+              select(MDR.School.ID, District.Rollup.ID,
+                     Demographics...Zipcode.Median.Income,
+                     Demographics...Poverty.Level,
+                     Is.Charter..Yes...No.,
+                     MDR.School.has.School.Account..Yes...No.,
+                     School.Address...Zipcode),
+            by = "MDR.School.ID") %>%
   rename(
     poverty = Demographics...Poverty.Level,
     income = Demographics...Zipcode.Median.Income,
     charter.school = Is.Charter..Yes...No.,
     school.account = MDR.School.has.School.Account..Yes...No.,
     zipcode = School.Address...Zipcode
-  )
+  ) %>%
+  mutate(charter.school = case_when(charter.school == "No" ~ 0,
+                                    charter.school == "Yes" ~ 1,
+                                    .default = NA),
+         school.account = case_when(school.account == "No" ~ 0,
+                                    school.account == "Yes" ~ 1,
+                                    .default = NA)) %>%
+  rename(Teacher.User.ID = Adult.User.ID)
 
-write_csv(df, "Data/df_clean.csv")
+write.csv(df, "Data/df_clean.csv")
