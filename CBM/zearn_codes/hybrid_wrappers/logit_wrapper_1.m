697420c9630eabb633904e95f855bfa78e8ca88e
Marcos Gallo <42765589+marcosfeng@users.noreply.github.com>
2024-05-14 08:12:56 -0400

    Fix QL refinement

---
+++ CBM/zearn_codes/hybrid_wrappers/logit_wrapper_1.m
@@ -0,0 +1,7 @@
+function [loglik] = logit_wrapper_1(parameters, subj)
+% Outcome variable: NNDSVD_student1
+subj.outcome = subj.NNDSVD_student1;
+% Action variable: NNDSVD_teacher2
+subj.action = subj.NNDSVD_teacher2;
+loglik = logit_model(parameters, subj);
+end
